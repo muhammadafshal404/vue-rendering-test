993904e5cd9fecdc5334a81941f1dafe4fea81a7
Mafshal444 <mafshal53@gmail.com>
2023-11-23 16:20:28 +0500

    first check if child renders or not.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :press="press"/>
+    <button @click="clickMe" > Press Me </button>
+    <input disabled v-model="press" />
   </div>
 </template>
 
@@ -10,8 +12,29 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      press: 1
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    clickMe(){
+      let vm = this
+      vm.press = vm.press+1
+      console.log(vm.press)
+    }
+  },
+  beforeUpdate(){
+    console.log("App beforeUpdate called.")
+  },
+  updated(){
+    console.log("App updated called.")
+  },
+  mounted(){
+    console.log("App mounted called.")
   }
 }
 </script>
